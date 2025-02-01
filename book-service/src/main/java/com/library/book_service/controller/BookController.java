4b1b1cf4d692f1a322651a97c484e2a5cc0725d7
charan006 <charankumar.chennareddy@gmail.com>
2025-02-01 17:30:53 +0530

    getBook api Added

--- book-service/src/main/java/com/library/book_service/controller/BookController.java
+++ book-service/src/main/java/com/library/book_service/controller/BookController.java
@@ -3,11 +3,14 @@ package com.library.book_service.controller;
 import com.library.book_service.model.Book;
 import com.library.book_service.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("book")
@@ -17,8 +20,13 @@ public class BookController {
     BookService bookService;
 
     @GetMapping("getBooks")
-    public List<Book> getAllBooks(){
+    public ResponseEntity<List<Book>> getAllBooks(){
         return bookService.getAllBooks();
+    }
+
+    @GetMapping("getBook/{id}")
+    public ResponseEntity<Book> getBook(@PathVariable int id){
+        return bookService.getBookWithID(id);
     }
 
 
--- book-service/src/main/java/com/library/book_service/service/BookService.java
+++ book-service/src/main/java/com/library/book_service/service/BookService.java
@@ -3,9 +3,12 @@ package com.library.book_service.service;
 import com.library.book_service.dao.BookDao;
 import com.library.book_service.model.Book;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BookService {
@@ -13,7 +16,14 @@ public class BookService {
     @Autowired
     BookDao bookDao;
 
-    public List<Book> getAllBooks() {
-        return bookDao.findAll();
+    public ResponseEntity<List<Book>> getAllBooks() {
+        return new ResponseEntity<>(bookDao.findAll(), HttpStatus.OK);
     }
+
+    public ResponseEntity<Book> getBookWithID(int id) {
+        return new ResponseEntity<>(bookDao.findById(id).get(), HttpStatus.OK);
+    }
+
+
+
 }
