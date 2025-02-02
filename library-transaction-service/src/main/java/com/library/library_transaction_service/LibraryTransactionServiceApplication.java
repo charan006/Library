3f5f84fe23a4e6705ea5354e6f3896cae26e7f1f
charan006 <charankumar.chennareddy@gmail.com>
2025-02-02 19:19:17 +0530

    Backup

---
+++ library-transaction-service/src/main/java/com/library/library_transaction_service/LibraryTransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.library.library_transaction_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryTransactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryTransactionServiceApplication.class, args);
+	}
+
+}
---
+++ library-transaction-service/src/test/java/com/library/library_transaction_service/LibraryTransactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.library.library_transaction_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryTransactionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
